a42d9a11ebfa8230f5f898a4e1dd446404d3d426
Johnny <jojozhuang@gmail.com>
2016-03-06 23:14:32 -0600

    hackerrank

--- HackerRank/Java/src/hackerrank/HackerRank.java
+++ HackerRank/Java/src/hackerrank/HackerRank.java
@@ -5,6 +5,10 @@
  */
 package hackerrank;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 /**
  *
  * @author Johnny
@@ -15,7 +19,46 @@ public class HackerRank {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int len = s1.length();
+            int x=sc.nextInt();
+            
+            for (int j = 0; j < 15 - len; j++) {
+                s1 += " ";
+            }
+            System.out.print(s1);
+            if (x < 10) {
+                System.out.print("00");                
+            } else if (x < 100) {
+                System.out.print("0");
+            }
+            System.out.print(x);
+            System.out.println();
+        }
+        System.out.println("================================");
+                */
+        
+        Deque<String> stack = new ArrayDeque<String>();
+        Deque<String> queue = new ArrayDeque<String>();
+
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        stack.push("D");
+
+        while (!stack.isEmpty())
+          System.out.println(stack.pop() + " ");
+
+        queue.add("A");
+        queue.add("B");
+        queue.add("C");
+        queue.add("D");
+        while (!queue.isEmpty())
+          System.out.println(queue.remove() + " ");
+      }
     }
-    
-}
