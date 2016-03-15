31f13a44f2ebe1a62d5c210c6b0958e0a9d6d84c
Johnny <jojozhuang@gmail.com>
2016-03-15 09:25:56 -0500

    hard

--- LeetCode/Java/src/leetcode/Solution135.java
+++ LeetCode/Java/src/leetcode/Solution135.java
@@ -18,7 +18,38 @@ package leetcode;
  * @author Johnny
  */
 public class Solution135 {
+    //http://www.programcreek.com/2014/03/leetcode-candy-java/
     public int candy(int[] ratings) {
-        return 0;
+        if (ratings == null || ratings.length == 0) {
+            return 0;
+        }
+
+        int[] candies = new int[ratings.length];
+        candies[0] = 1;
+
+        //from let to right
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            } else { 
+                // if not ascending, assign 1
+                candies[i] = 1;
+            }
+        }
+
+        int result = candies[ratings.length - 1];
+
+        //from right to left
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            int cur = 1;
+            if (ratings[i] > ratings[i + 1]) {
+                cur = candies[i + 1] + 1;
+            }
+
+            result += Math.max(cur, candies[i]);
+            candies[i] = cur;
+        }
+
+        return result;
     }
 }
---
+++ LeetCode/Java/test/leetcode/Solution312Test.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Johnny
+ */
+public class Solution312Test {
+    
+    public Solution312Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of maxCoins method, of class Solution312.
+     */
+    @Test
+    public void testMaxCoins() {
+        System.out.println("maxCoins");
+        int[] nums = null;
+        Solution312 instance = new Solution312();
+        int expResult = 0;
+        int result = instance.maxCoins(nums);
+        assertEquals(expResult, result);
+        
+        assertEquals(167, instance.maxCoins(new int[]{3, 1, 5, 8}));
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
