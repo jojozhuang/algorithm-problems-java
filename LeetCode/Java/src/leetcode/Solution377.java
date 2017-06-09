ab2bf70b6391c975bceab7af7c9320fe76e1a5e3
Zhuang, Rong <jojozhuang@gmail.com>
2017-06-08 22:42:12 -0700

    new questions

---
+++ LeetCode/Java/src/leetcode/Solution377.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+/**
+ *
+ * @author Johnny
+ */
+public class Solution377 {
+    public int combinationSum4(int[] nums, int target) {
+        return 0;
+    }
+}
