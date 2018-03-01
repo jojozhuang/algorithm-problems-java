18a84a724608e438e7e3b28753d4f6d54bff5340
Zhuang, Rong <jojozhuang@gmail.com>
2018-02-28 16:14:46 -0800

    new questions

---
+++ LeetCode/src/johnny/algorithm/leetcode/Solution767.java
@@ -0,0 +1,26 @@
+package johnny.algorithm.leetcode;
+
+/**
+ *767. Reorganize String
+A Given a string S, check if the letters can be rearranged so that two characters that are adjacent to each other are not the same.
+
+If possible, output any possible result.  If not possible, return the empty string.
+
+Example 1:
+
+Input: S = "aab"
+Output: "aba"
+Example 2:
+
+Input: S = "aaab"
+Output: ""
+Note:
+
+S will consist of lowercase letters and have length in range [1, 500].
+ * @author Johnny
+ */
+public class Solution767 {
+    public String reorganizeString(String S) {
+        return "";
+    }
+}
