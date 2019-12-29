bcfd04f3cdfbb882b5e192f1077d11082a33516b
jojozhuang <jojozhuang@gmail.com>
2019-12-28 22:30:11 -0800

    build on windows

--- lintcode/lintcode-algorithm/test/johnny/lintcode/algorithm/test/LongestConsecutiveTest.java
+++ lintcode/lintcode-algorithm/test/johnny/lintcode/algorithm/test/LongestConsecutiveTest.java
@@ -41,10 +41,10 @@ public class LongestConsecutiveTest {
         assertEquals(0, instance.longestConsecutive(null));
 
         TreeNode root2 = TreeNode.createInstance("1,#,3,2,4,#,#,#,5");
-        //assertEquals(3, instance.longestConsecutive(root2));
+        assertEquals(3, instance.longestConsecutive(root2));
 
         TreeNode root3 = TreeNode.createInstance("2,#,3,2,#,1,#");
-        //assertEquals(2, instance.longestConsecutive(root3));
+        assertEquals(2, instance.longestConsecutive(root3));
 
         TreeNode root4 = TreeNode.createInstance("1,2,3,4,5,#,#");
         assertEquals(2, instance.longestConsecutive(root4));
