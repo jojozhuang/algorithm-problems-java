4593ace76ee9ecf2f4de23f4b30a4e5845d2c728
Zhuang, Rong <jojozhuang@gmail.com>
2020-02-24 08:40:03 -0800

    refine solution1363

--- leetcode-algorithm-java/src/main/java/johnny/leetcode/algorithm/SolutionA1363.java
+++ leetcode-algorithm-java/src/main/java/johnny/leetcode/algorithm/SolutionA1363.java
@@ -45,6 +45,91 @@ public class SolutionA1363 {
         if (sum == 0) {
             return "0";
         }
+
+        if (sum % 3 == 0) {
+            // go to build string
+        } else if (sum % 3 == 1 && (count[1] + count[4] + count[7] > 0)) {
+            // remove one digit of 1,4,7
+            if (count[1] > 0) {
+                count[1]--;
+                sum -= 1;
+            } else if (count[4] > 0) {
+                count[4]--;
+                sum -= 4;
+            } else if (count[7] > 0) {
+                count[7]--;
+                sum -= 7;
+            }
+        } else if (sum % 3 == 2 && (count[2] + count[5] + count[8] > 0)) {
+            // remove one digit of 2,5,8
+            if (count[2] > 0) {
+                count[2]--;
+                sum -= 2;
+            } else if (count[5] > 0) {
+                count[5]--;
+                sum -= 5;
+            } else if (count[8] > 0) {
+                count[8]--;
+                sum -= 8;
+            }
+        } else if (sum % 3 == 2 && (count[1] + count[4] + count[7] > 1)) {
+            // remove two digits of 1,4,7
+            int[] diff = new int[]{1,4,7};
+            int i = 0;
+            while (i < 3) {
+                if (count[diff[i]] > 0) {
+                    count[diff[i]]--;
+                    sum -= diff[i];
+                } else {
+                    i++;
+                }
+                if (sum % 3 == 0) {
+                    break;
+                }
+            }
+        } else if (sum % 3 == 1 && (count[2] + count[5] + count[8] > 1)) {
+            // remove two digits of 2,5,8
+            int[] diff = new int[]{2,5,8};
+            int i = 0;
+            while (i < 3) {
+                if (count[diff[i]] > 0) {
+                    count[diff[i]]--;
+                    sum -= diff[i];
+                } else {
+                    i++;
+                }
+                if (sum % 3 == 0) {
+                    break;
+                }
+            }
+        }
+
+        if (sum % 3 != 0) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) {
+            while (count[i] > 0) {
+                sb.append(i);
+                count[i]--;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public String largestMultipleOfThree2(int[] digits) {
+        int sum = 0;
+        int[] count = new int[10];
+        for (int i : digits) {
+            count[i]++;
+            sum += i;
+        }
+
+        if (sum == 0) {
+            return "0";
+        }
         if (sum % 3 == 0) {
             return solve(count, sum, 0);
         }
--- leetcode-algorithm-java/src/test/java/johnny/leetcode/algorithm/SolutionA1363Test.java
+++ leetcode-algorithm-java/src/test/java/johnny/leetcode/algorithm/SolutionA1363Test.java
@@ -11,12 +11,14 @@ public class SolutionA1363Test extends JunitBase {
         System.out.println("largestMultipleOfThree");
         SolutionA1363 instance = new SolutionA1363();
 
-        //assertEquals("8760", instance.largestMultipleOfThree(new int[]{8,6,7,1,0}));
+        assertEquals("8760", instance.largestMultipleOfThree(new int[]{8,6,7,1,0}));
         assertEquals("", instance.largestMultipleOfThree(new int[]{5,8}));
-        /*assertEquals("981", instance.largestMultipleOfThree(new int[]{8,1,9}));
+        assertEquals("981", instance.largestMultipleOfThree(new int[]{8,1,9}));
         assertEquals("", instance.largestMultipleOfThree(new int[]{1}));
         assertEquals("0", instance.largestMultipleOfThree(new int[]{0,0,0,0,0,0}));
         assertEquals("111", instance.largestMultipleOfThree(new int[]{1,1,1,2}));
-        assertEquals("966", instance.largestMultipleOfThree(new int[]{9,8,6,8,6}));*/
+        assertEquals("966", instance.largestMultipleOfThree(new int[]{9,8,6,8,6}));
+        assertEquals("3", instance.largestMultipleOfThree(new int[]{3,1,4}));
+        assertEquals("6", instance.largestMultipleOfThree(new int[]{6,2,5}));
     }
 }
